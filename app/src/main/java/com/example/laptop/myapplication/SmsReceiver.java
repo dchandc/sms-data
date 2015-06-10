ca2dcafb79741639d6e7d94497448a7a583ca5ca
dchimeraan <dennischandc@gmail.com>
2015-06-10 15:36:05 -0700

    Add MessageBuffer class

--- app/src/main/java/com/example/laptop/myapplication/SmsReceiver.java
+++ app/src/main/java/com/example/laptop/myapplication/SmsReceiver.java
@@ -14,6 +14,9 @@ import android.util.Base64;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class SmsReceiver extends BroadcastReceiver{
     MainActivity main_act;
@@ -21,10 +24,13 @@ public class SmsReceiver extends BroadcastReceiver{
     DatagramSocket socket;
     String googleDns = "8.8.8.8";
     int dnsPort = 53;
-    int bytesPerSms = 118;
+    int bytesPerSms = 115;
+    ArrayList<MessageBuffer> mbufList;
+    int seqNum = 0;
 
     public SmsReceiver() {
         smsManager = SmsManager.getDefault();
+        mbufList = new ArrayList<MessageBuffer>();
         try {
             socket = new DatagramSocket();
         } catch (Exception e) {
@@ -38,15 +44,80 @@ public class SmsReceiver extends BroadcastReceiver{
         Bundle bundle = intent.getExtras();
         if(bundle != null)
         {
-            Object[] data = (Object[]) bundle.get("pdus");
-            for (int j=0; j < data.length; j++){
-                SmsMessage sms = SmsMessage.createFromPdu((byte[])data[j]);
-                SmsAsyncTask task = new SmsAsyncTask(sms);
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            if (pdus.length < 1)
+                return;
+
+            String from = SmsMessage.createFromPdu((byte[]) pdus[0]).getOriginatingAddress();
+            StringBuilder sb = new StringBuilder("");
+            for (int j = 0; j < pdus.length; j++) {
+                SmsMessage sms = SmsMessage.createFromPdu((byte[]) pdus[j]);
+                sb.append(sms.getMessageBody());
+            }
+
+            String body = sb.toString();
+            main_act.appendText("SMS message from " +
+                    from + " [" + body.length() + "]: " +
+                    body);
+
+            byte[] raw = Base64.decode(body, Base64.DEFAULT);
+            sb = new StringBuilder("Byte64-decoded SMS message [" + raw.length + "]: ");
+            for (int i = 0; i < raw.length; i++) {
+                sb.append(String.format("%02x ", raw[i]));
+            }
+            sb.append("\n");
+            main_act.appendText(sb.toString());
+
+            if (raw.length < 3)
+                return;
+
+            int seqNum = raw[0];
+            int dataNum = raw[1];
+            int dataCount = raw[2];
+            Log.i("sms", seqNum + " " + dataNum + " " + dataCount);
+            // dataNum and dataCount start at 1
+            if (seqNum < 0 || dataNum < 1 || dataCount < 1 || dataNum > dataCount)
+                return;
+
+            byte[] data = new byte[raw.length - 3];
+            System.arraycopy(raw, 3, data, 0, raw.length - 3);
+
+            boolean found = false;
+            MessageBuffer mbuf = null;
+            int mbufIndex = -1;
+            for (int i = 0; i < mbufList.size(); i++) {
+                mbuf = mbufList.get(i);
+                if (mbuf.seqNum == seqNum) {
+                    found = true;
+                    mbuf.add(data, dataNum);
+                    mbufIndex = i;
+                    Log.i("sms", "Found mbuf with seqNum " + seqNum);
+                    break;
+                }
+            }
+
+            if (!found) {
+                mbuf = new MessageBuffer(seqNum);
+                mbuf.add(data, dataNum);
+                mbufList.add(mbuf);
+                mbufIndex = mbufList.size() - 1;
+                Log.i("sms", "Added mbuf with seqNum " + seqNum);
+            }
+
+            if (mbuf.count == dataCount) {
+                byte[] mergedData = mbuf.getData();
+                sb = new StringBuilder("");
+                for (int i = 0; i < mergedData.length; i++) {
+                    sb.append(String.format("%02x", mergedData[i]) + " ");
+                }
+                Log.i("sms", "Merged data[" + mergedData.length + "]: " + sb.toString());
+                SmsAsyncTask task = new SmsAsyncTask(seqNum++, from, mergedData);
                 if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
                     task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
                 else
                     task.execute();
                 Log.i("SmsReceiver", "Executed new SmsAsyncTask");
+                mbufList.remove(mbufIndex);
             }
         }
     }
@@ -57,54 +128,42 @@ public class SmsReceiver extends BroadcastReceiver{
 
 
     public class SmsAsyncTask extends AsyncTask<Void, String, Void> {
-        SmsMessage sms;
-        public SmsAsyncTask(SmsMessage sms) {
-            this.sms = sms;
+        String from;
+        byte[] mergedData;
+        int seqNum;
+
+        public SmsAsyncTask(int seqNum, String from, byte[] mergedData) {
+            this.seqNum = seqNum;
+            this.from = from;
+            this.mergedData = mergedData;
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
-            String from = sms.getOriginatingAddress();
-            String body = sms.getMessageBody();
-
-            StringBuilder sb = new StringBuilder("SMS message from " +
-                    from + " [" + body.length() + "]: " +
-                    body);
-            publishProgress(sb.toString());
-
-            byte[] raw = Base64.decode(sms.getMessageBody(), Base64.DEFAULT);
-
-            sb = new StringBuilder("Byte64-decoded SMS message [" + raw.length + "]: ");
-            for (int i = 0; i < raw.length; i++) {
-                sb.append(String.format("%02x ", raw[i]));
-            }
-            sb.append("\n");
-            publishProgress(sb.toString());
-
-            int queryLength = raw.length - 28;
+            int queryLength = mergedData.length - 28;
             if (queryLength < 0)
                 return null;
 
             byte[] query = new byte[queryLength];
-            System.arraycopy(raw, 28, query, 0, queryLength);
+            System.arraycopy(mergedData, 28, query, 0, queryLength);
             DatagramPacket packet = new DatagramPacket(query, query.length);
 
             try {
                 InetAddress destAddress;
-                int destPort = (raw[22] & 0xff) << 8 | (raw[23] & 0xff);
+                int destPort = (mergedData[22] & 0xff) << 8 | (mergedData[23] & 0xff);
                 if (destPort == dnsPort) {
                     destAddress = InetAddress.getByName(googleDns);
                 } else {
                     String addressString = String.format("%d.%d.%d.%d",
-                            raw[16] & 0xff, raw[17] & 0xff,
-                            raw[18] & 0xff, raw[19] & 0xff);
+                            mergedData[16] & 0xff, mergedData[17] & 0xff,
+                            mergedData[18] & 0xff, mergedData[19] & 0xff);
                     destAddress = InetAddress.getByName(addressString);
                 }
                 Log.i("SmsAsyncTask", "Destination address " + destAddress.toString());
                 packet.setAddress(destAddress);
                 packet.setPort(destPort);
 
-                sb = new StringBuilder("");
+                StringBuilder sb = new StringBuilder("");
                 for (int i = 0; i < query.length; i++) {
                     sb.append(String.format("%02x", query[i]) + " ");
                 }
@@ -123,13 +182,18 @@ public class SmsReceiver extends BroadcastReceiver{
                 Log.i("SmsAsyncTask", "Recv packet [" + packet.getLength() + "]: " + sb.toString());
                 publishProgress(sb.toString());
 
-                for (int i = 0; i < (packet.getLength() / bytesPerSms) + 1; i++) {
+                int count = (packet.getLength() / bytesPerSms) + 1;
+                for (int i = 0; i < count; i++) {
                     int offset = i * bytesPerSms;
-                    int len = (packet.getLength() - offset) % bytesPerSms;
-                    byte[] sub = new byte[len];
-                    System.arraycopy(buffer, offset, sub, 0, len);
-                    String msg = Base64.encodeToString(sub, Base64.DEFAULT);
-                    publishProgress("Byte64-encoded SMS message [" + msg.length() + "]: " + msg);
+                    int len = Math.min(packet.getLength() - offset, bytesPerSms);
+                    byte[] sub = new byte[3 + len];
+                    sub[0] = (byte) seqNum;
+                    sub[1] = (byte) (i + 1);
+                    sub[2] = (byte) count;
+                    System.arraycopy(buffer, offset, sub, 3, len);
+                    String msg = Base64.encodeToString(sub, Base64.NO_WRAP);
+                    publishProgress("Byte64-encoded SMS message [" + msg.length() + "]: " +
+                            msg + "\n");
                     smsManager.sendTextMessage(from, null, msg, null, null);
                 }
             } catch (Exception e) {
@@ -144,6 +208,72 @@ public class SmsReceiver extends BroadcastReceiver{
         protected void onProgressUpdate(String... message) {
             if (message != null && message.length > 0)
                 main_act.appendText(message[0]);
+        }
+    }
+
+    public class MessageBuffer {
+        ArrayList<Entry> list;
+        int count;
+        int length;
+        int seqNum;
+
+        class Entry {
+            byte[] data;
+            int dataNum;
+
+            Entry(byte[] data, int dataNum) {
+                this.data = data;
+                this.dataNum = dataNum;
+            }
+        }
+
+        public MessageBuffer(int seqNum) {
+            this.seqNum = seqNum;
+            list = new ArrayList<Entry>();
+            count = 0;
+            length = 0;
+        }
+
+        public void add(byte[] data, int dataNum) {
+            if (data == null || dataNum < 1)
+                return;
+
+            // Reject duplicates
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).dataNum == dataNum)
+                    return;
+            }
+
+            list.add(new Entry(data, dataNum));
+            length += data.length;
+            count++;
+        }
+
+        public byte[] getData() {
+            if (list.size() < 1)
+                return null;
+
+            Collections.sort(list, new Comparator<Entry>() {
+                @Override
+                public int compare(Entry first, Entry second) {
+                    if (first.dataNum < second.dataNum)
+                        return -1;
+                    else if (first.dataNum > second.dataNum)
+                        return 1;
+                    else
+                        return 0;
+                }
+            });
+
+            byte[] mergedData = new byte[length];
+            int offset = 0;
+            for (int i = 0; i < list.size(); i++){
+                byte[] b = list.get(i).data;
+                System.arraycopy(b, 0, mergedData, offset, b.length);
+                offset += b.length;
+            }
+
+            return mergedData;
         }
     }
 }
--- app/src/main/java/com/example/laptop/myapplication/SmsTask.java
+++ app/src/main/java/com/example/laptop/myapplication/SmsTask.java
@@ -25,6 +25,7 @@ public class SmsTask extends AsyncTask<Void, Void, Void> {
         try {
             String msg;
 
+            /*
             byte [] all = new byte[100];
             for(int i = 0; i < 100; i++)
             {
@@ -34,7 +35,8 @@ public class SmsTask extends AsyncTask<Void, Void, Void> {
             Log.i("sms", "Sending message [" + msg.length() + "]: " + msg);
             smsManager.sendTextMessage(phoneNo, null, msg, null, null);
             Log.i("sms", "Message sent");
-            /*
+            */
+
             byte [] all = new byte[64];
             all[0] = 100;
             all[1] = 1;
@@ -43,43 +45,23 @@ public class SmsTask extends AsyncTask<Void, Void, Void> {
             {
                 all[i] = (byte) i;
             }
-            msg = Base64.encodeToString(all, Base64.DEFAULT);
-            Log.i("sms", "Sending message: " + msg);
+            msg = Base64.encodeToString(all, Base64.NO_WRAP);
+            Log.i("sms", "Sending message [" + msg.length() + "]: " + msg);
             smsManager.sendTextMessage(phoneNo, null, msg, null, null);
             Log.i("sms", "Message sent");
 
-            all[0] = 101;
+            all[0] = 100;
             all[1] = 2;
-            all[2] = 3;
-            msg = Base64.encodeToString(all, Base64.DEFAULT);
-            Log.i("sms", "Sending message: " + msg);
+            all[2] = 2;
+            for(int i = 3; i < 64; i++)
+            {
+                all[i] = (byte) i;
+            }
+            msg = Base64.encodeToString(all, Base64.NO_WRAP);
+            Log.i("sms", "Sending message [" + msg.length() + "]: " + msg);
             smsManager.sendTextMessage(phoneNo, null, msg, null, null);
             Log.i("sms", "Message sent");
 
-            all[0] = 102;
-            all[1] = 1;
-            all[2] = 1;
-            msg = Base64.encodeToString(all, Base64.DEFAULT);
-            Log.i("sms", "Sending message: " + msg);
-            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
-            Log.i("sms", "Message sent");
-
-            all[0] = 101;
-            all[1] = 1;
-            all[2] = 3;
-            msg = Base64.encodeToString(all, Base64.DEFAULT);
-            Log.i("sms", "Sending message: " + msg);
-            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
-            Log.i("sms", "Message sent");
-
-            all[0] = 101;
-            all[1] = 3;
-            all[2] = 3;
-            msg = Base64.encodeToString(all, Base64.DEFAULT);
-            Log.i("sms", "Sending message: " + msg);
-            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
-            Log.i("sms", "Message sent");
-            */
         } catch (Exception e) {
             e.printStackTrace();
             Log.i("sms", "Exception");
